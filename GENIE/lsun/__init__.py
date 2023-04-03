360d7545a9ab3babe0df307bfa156df097d4655d
quandm7 <v.quandm7@vinai.io>
2023-04-03 20:50:18 +0000

    remove unneed things

--- GENIE/lsun/__init__.py
+++
@@ -1,9 +0,0 @@
-# ---------------------------------------------------------------
-# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
-# ---------------------------------------------------------------
-
-""" Originated from https://github.com/fyu/lsun
-The license for the original version of this file can be found in this directory (LICENSE_MIT).
-"""
-
-# empty
