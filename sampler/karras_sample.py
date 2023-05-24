404972eda93e0b2f6d991426a0f8e45897e0d1fe
haopt12 <v.haopt12@vinai.io>
2023-05-24 12:47:46 +0700

    update

--- sampler/karras_sample.py
+++ sampler/karras_sample.py
@@ -32,13 +32,6 @@ def karras_sample(
     if generator is None:
         generator = get_generator("dummy")
 
-    # if sampler == "progdist":
-    #     sigmas = get_sigmas_karras(steps + 1, sigma_min, sigma_max, rho, device=device)
-    # else:
-    #     sigmas = get_sigmas_karras(steps, sigma_min, sigma_max, rho, device=device)
-
-    # x_T = generator.randn(*shape, device=device) * sigma_max
-
     sigmas = th.linspace(sigma_max, sigma_min, steps, device=device)
 
     sample_fn = {
@@ -49,7 +42,6 @@ def karras_sample(
         "progdist": sample_progdist,
         "euler": sample_euler,
         "multistep": stochastic_iterative_sampler,
-        "stochastic": karras_stochastic_sampler,
     }[sampler]
 
     if sampler in ["heun", "dpm"]:
@@ -109,117 +101,10 @@ def karras_sample(
     return x_0
 
 
-def get_sigmas_karras(n, sigma_min, sigma_max, rho=7.0, device="cpu"):
-    """Constructs the noise schedule of Karras et al. (2022)."""
-    ramp = th.linspace(0, 1, n)
-    min_inv_rho = sigma_min ** (1 / rho)
-    max_inv_rho = sigma_max ** (1 / rho)
-    sigmas = (max_inv_rho + ramp * (min_inv_rho - max_inv_rho)) ** rho
-    return append_zero(sigmas).to(device)
-
-
 def to_d(x, sigma, denoised):
     """Converts a denoiser output to a Karras ODE derivative."""
     # return (x - denoised) / append_dims(sigma, x.ndim)
     return x # identity for flow matching
-
-
-def get_ancestral_step(sigma_from, sigma_to):
-    """Calculates the noise level (sigma_down) to step down to and the amount
-    of noise to add (sigma_up) when doing an ancestral sampling step."""
-    sigma_up = (
-        sigma_to**2 * (sigma_from**2 - sigma_to**2) / sigma_from**2
-    ) ** 0.5
-    sigma_down = (sigma_to**2 - sigma_up**2) ** 0.5
-    return sigma_down, sigma_up
-
-
-@th.no_grad()
-def sample_euler_ancestral(model, x, sigmas, generator, progress=False, callback=None):
-    """Ancestral sampling with Euler method steps."""
-    s_in = x.new_ones([x.shape[0]])
-    indices = range(len(sigmas) - 1)
-    if progress:
-        from tqdm.auto import tqdm
-
-        indices = tqdm(indices)
-
-    for i in indices:
-        denoised = model(x, sigmas[i] * s_in)
-        sigma_down, sigma_up = get_ancestral_step(sigmas[i], sigmas[i + 1])
-        if callback is not None:
-            callback(
-                {
-                    "x": x,
-                    "i": i,
-                    "sigma": sigmas[i],
-                    "sigma_hat": sigmas[i],
-                    "denoised": denoised,
-                }
-            )
-        d = to_d(x, sigmas[i], denoised)
-        # Euler method
-        dt = sigma_down - sigmas[i]
-        x = x + d * dt
-        x = x + generator.randn_like(x) * sigma_up
-    return x
-
-
-@th.no_grad()
-def sample_heun(
-    denoiser,
-    x,
-    sigmas,
-    generator,
-    progress=False,
-    callback=None,
-    s_churn=0.0,
-    s_tmin=0.0,
-    s_tmax=float("inf"),
-    s_noise=1.0,
-):
-    """Implements Algorithm 2 (Heun steps) from Karras et al. (2022)."""
-    s_in = x.new_ones([x.shape[0]])
-    indices = range(len(sigmas) - 1)
-    if progress:
-        from tqdm.auto import tqdm
-
-        indices = tqdm(indices)
-
-    for i in indices:
-        gamma = (
-            min(s_churn / (len(sigmas) - 1), 2**0.5 - 1)
-            if s_tmin <= sigmas[i] <= s_tmax
-            else 0.0
-        )
-        eps = generator.randn_like(x) * s_noise
-        sigma_hat = sigmas[i] * (gamma + 1)
-        if gamma > 0:
-            x = x + eps * (sigma_hat**2 - sigmas[i] ** 2) ** 0.5
-        denoised = denoiser(x, sigma_hat * s_in)
-        d = to_d(x, sigma_hat, denoised)
-        if callback is not None:
-            callback(
-                {
-                    "x": x,
-                    "i": i,
-                    "sigma": sigmas[i],
-                    "sigma_hat": sigma_hat,
-                    "denoised": denoised,
-                }
-            )
-        dt = sigmas[i + 1] - sigma_hat
-        if sigmas[i + 1] == 0:
-            # Euler method
-            x = x + d * dt
-        else:
-            # Heun's method
-            x_2 = x + d * dt
-            denoised_2 = denoiser(x_2, sigmas[i + 1] * s_in)
-            d_2 = to_d(x_2, sigmas[i + 1], denoised_2)
-            d_prime = (d + d_2) / 2
-            x = x + d_prime * dt
-    return x
 
 
 @th.no_grad()
@@ -259,103 +144,7 @@ def sample_euler(
 
 
 @th.no_grad()
-def sample_dpm(
-    denoiser,
-    x,
-    sigmas,
-    generator,
-    progress=False,
-    callback=None,
-    s_churn=0.0,
-    s_tmin=0.0,
-    s_tmax=float("inf"),
-    s_noise=1.0,
-):
-    """A sampler inspired by DPM-Solver-2 and Algorithm 2 from Karras et al. (2022)."""
-    s_in = x.new_ones([x.shape[0]])
-    indices = range(len(sigmas) - 1)
-    if progress:
-        from tqdm.auto import tqdm
-
-        indices = tqdm(indices)
-
-    for i in indices:
-        gamma = (
-            min(s_churn / (len(sigmas) - 1), 2**0.5 - 1)
-            if s_tmin <= sigmas[i] <= s_tmax
-            else 0.0
-        )
-        eps = generator.randn_like(x) * s_noise
-        sigma_hat = sigmas[i] * (gamma + 1)
-        if gamma > 0:
-            x = x + eps * (sigma_hat**2 - sigmas[i] ** 2) ** 0.5
-        denoised = denoiser(x, sigma_hat * s_in)
-        d = to_d(x, sigma_hat, denoised)
-        if callback is not None:
-            callback(
-                {
-                    "x": x,
-                    "i": i,
-                    "sigma": sigmas[i],
-                    "sigma_hat": sigma_hat,
-                    "denoised": denoised,
-                }
-            )
-        # Midpoint method, where the midpoint is chosen according to a rho=3 Karras schedule
-        sigma_mid = ((sigma_hat ** (1 / 3) + sigmas[i + 1] ** (1 / 3)) / 2) ** 3
-        dt_1 = sigma_mid - sigma_hat
-        dt_2 = sigmas[i + 1] - sigma_hat
-        x_2 = x + d * dt_1
-        denoised_2 = denoiser(x_2, sigma_mid * s_in)
-        d_2 = to_d(x_2, sigma_mid, denoised_2)
-        x = x + d_2 * dt_2
-    return x
-
-
-@th.no_grad()
-def sample_onestep(
-    distiller,
-    x,
-    sigmas,
-    generator=None,
-    progress=False,
-    callback=None,
-):
-    """Single-step generation from a distilled model."""
-    s_in = x.new_ones([x.shape[0]])
-    return x - distiller(x, sigmas[0] * s_in)
-
-
-@th.no_grad()
-def stochastic_iterative_sampler(
-    distiller,
-    x,
-    sigmas,
-    generator,
-    ts,
-    progress=False,
-    callback=None,
-    t_min=0.002,
-    t_max=80.0,
-    rho=7.0,
-    steps=40,
-):
-    t_max_rho = t_max ** (1 / rho)
-    t_min_rho = t_min ** (1 / rho)
-    s_in = x.new_ones([x.shape[0]])
-
-    for i in range(len(ts) - 1):
-        t = (t_max_rho + ts[i] / (steps - 1) * (t_min_rho - t_max_rho)) ** rho
-        # x0 = distiller(x, t * s_in)
-        next_t = (t_max_rho + ts[i + 1] / (steps - 1) * (t_min_rho - t_max_rho)) ** rho
-        next_t = np.clip(next_t, t_min, t_max)
-        x0 = x + (next_t - t) * distiller(x, t * s_in)
-        x = x0 + generator.randn_like(x) * np.sqrt(next_t**2 - t_min**2)
-
-    return x
-
-
-def karras_stochastic_sampler(
+def sample_heun(
     distiller,
     x,
     sigmas,
@@ -405,40 +194,3 @@ def karras_stochastic_sampler(
             x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
     
     return x_next
-
-
-@th.no_grad()
-def sample_progdist(
-    denoiser,
-    x,
-    sigmas,
-    generator=None,
-    progress=False,
-    callback=None,
-):
-    s_in = x.new_ones([x.shape[0]])
-    sigmas = sigmas[:-1]  # skip the zero sigma
-
-    indices = range(len(sigmas) - 1)
-    if progress:
-        from tqdm.auto import tqdm
-
-        indices = tqdm(indices)
-
-    for i in indices:
-        sigma = sigmas[i]
-        denoised = denoiser(x, sigma * s_in)
-        d = to_d(x, sigma, denoised)
-        if callback is not None:
-            callback(
-                {
-                    "x": x,
-                    "i": i,
-                    "sigma": sigma,
-                    "denoised": denoised,
-                }
-            )
-        dt = sigmas[i + 1] - sigma
-        x = x + d * dt
-
-    return x
